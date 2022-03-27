61ddb33987b5caca66defc67b30a89d294dd54a5
cechareb <cesar.chahuasrebatta@gmail.com>
2022-03-27 17:54:12 -0500

    Flask docker-compose postgres pgadmin

---
+++ web/main.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify
+from sqlalchemy import create_engine
+from faker import Faker
+app = Flask(__name__)
+connection_db = "postgresql://postgres_user:postgres_pwd@db:5432/postgresdb"
+app.config["SQLALCHEMY_DATABASE_URI"] = connection_db
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+engine = create_engine(connection_db)
+
+@app.route('/insertar_datos',methods=["POST"])
+def insertar_datos():
+    fake = Faker()
+    try:
+        for i in range(10):
+            nombre = fake.name()
+            telefono = fake.phone_number()
+            engine.execute(f"""INSERT INTO usuarios(nombre,telefono) values('{nombre}','{telefono}')""")
+    except:
+        return jsonify({"Respuesta":"No se inserto"})
+    return jsonify({"Respuesta":"Datos insertado!!"})
+
+@app.route('/creartabla',methods=["POST"])
+def crear_tabla():
+    try:
+        engine.execute("CREATE TABLE IF NOT EXISTS usuarios(id serial, nombre varchar(200), telefono varchar(200))")
+    except:
+        return jsonify({"Respuesta":"FALLE"})
+    return jsonify({"Respuesta":"Tabla creada exitosamente"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=80)
